85edff1a46e73cd5ab0f30808a9f4aa0a58479e4
Angelina Tan <angiedotpangie@gmail.com>
2022-06-02 15:08:18 +0900

    Add function to return result of match

--- myScript.js
+++ myScript.js
@@ -1,26 +1,70 @@
-/*
-Create function called computerPlay that will randomly return 'Rock', 'Paper' or 'Scissors'.
-*/
 
+// Function that will randomly return 'Rock', 'Paper' or 'Scissors'.
 function computerPlay() {
 
-    // return random value between 0 to 2
-    let randomValue = Math.floor(Math.random() * 3); 
+    // return random value between 1 to 3
+    let randomValue = Math.floor(Math.random() * 3 + 1); 
 
-    // print String in accordance to outcome of randomValue
+    // return String in accordance to outcome of randomValue
     switch (randomValue) {
 
-        case 0:
-            console.log('Rock');
-            break;
         case 1:
-            console.log('Paper');
+            return "Rock"
             break;
         case 2:
-            console.log('Scissors');
+            return "Paper"
+            break;
+        case 3:
+            return "Scissors"
 
     }
 
 }
 
-computerPlay();
+console.log(computerPlay());
+
+/*
+Create function that plays single round of RPS. Takes two parameters, then returns string that declares winner of round. Input to be case-insensitive.
+
+Rock beats Scissors
+Scissors beats Paper
+Paper beats Rock
+
+*/
+
+function playRound(playerSelection, computerSelection) {
+
+    // return String of Win, Lose or Tie result
+    switch(true) {
+
+        // Win Conditions
+        case playerSelection == "Rock" && computerSelection == "Scissors":
+        case playerSelection == "Scissors" && computerSelection == "Paper":
+        case playerSelection == "Paper" && computerSelection == "Rock":
+
+            return "You Win!";
+            break;
+        
+        // Lose Conditions
+        case playerSelection == "Rock" && computerSelection == "Paper":
+        case playerSelection == "Scissors" && computerSelection == "Rock":
+        case playerSelection == "Paper" && computerSelection == "Scissors":
+
+            return "You Lose!";
+            break;
+
+        // Tie Condition
+        case playerSelection === computerSelection:
+
+            return "It\'s a Tie!"
+            break;
+    }
+
+
+    
+
+}
+
+let result = playRound("Rock", "Rock")
+
+console.log(result);
